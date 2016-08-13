9687e99a47a4f427e7de2761679a175ee5611005
dannywo <dannywo@gmail.com>
2016-08-12 18:13:55 -0700

    Added project files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Problem: User interaction doesn't provide desired results.
+// Solution: Add interactivity so the user can manage daily tasks
+
+// Add a new task
+	// When the button is pressed
+	// Create a task
+
+
+
+// Edit an existing task
+
+
+
+// Delete an existing task
+
+
+
+
+// Mark a task as complete
+
+
+
+
+// Mark a task as incomplete
